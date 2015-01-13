021aa243db792347f3b431cbeaa97c55676ad76f
ahueni <ahueni@geo.uzh.ch>
2015-01-13 14:00:41 +0100

    Update to optionally use radiometric information for the cluster generation

--- LoadingDialog.m
+++ LoadingDialog.m
@@ -10,14 +10,15 @@ function LoadingDialog(hObject, EventData)
 
 
      % create new window
-    dlg_user_data.window_h = figure('Units', 'normalized', 'Position', [0 0 0.5 0.5], 'Name', 'SALSA++ Loading Dialog', 'Color', [0.9 0.9 0.9]);
+    dlg_user_data.window_h = figure('Units', 'normalized', 'Position', [0 0 0.8 0.5], 'Name', 'SALSA++ Loading Dialog', 'Color', [0.9 0.9 0.9]);
 
     set(dlg_user_data.window_h,'Toolbar','figure');
     
     
     col1_pos = 0.05;
-    col2_pos = 0.5;
-    axes_width = 0.4;
+    col2_pos = 0.4;
+    col3_pos = 0.73;
+    axes_width = 0.25;
     axes_height = 0.3;
     row1_pos = 0.90 - axes_height;
     row2_pos = 0.4 - axes_height;
@@ -31,14 +32,15 @@ function LoadingDialog(hObject, EventData)
     
     dlg_user_data.time_cluster_axes = axes('Parent',dlg_user_data.window_h,'Position',[col1_pos row1_pos axes_width axes_height]);  
     dlg_user_data.cal_cluster_axes = axes('Parent',dlg_user_data.window_h,'Position',[col2_pos row1_pos axes_width axes_height]); 
-    dlg_user_data.combined_cluster_axes = axes('Parent',dlg_user_data.window_h,'Position',[col2_pos row2_pos axes_width axes_height]); 
     
+    dlg_user_data.combined_cluster_axes = axes('Parent',dlg_user_data.window_h,'Position',[col3_pos row1_pos axes_width axes_height]); 
     
+    dlg_user_data.rad_cluster_axes = axes('Parent',dlg_user_data.window_h,'Position',[col2_pos row2_pos axes_width*2.3 axes_height]); 
     
     % sliders
     dlg_user_data.cluster_time_slider = javax.swing.JSlider(javax.swing.SwingConstants.HORIZONTAL, 1, 360, 10);
 
-    slider = jcontrol(dlg_user_data.window_h, dlg_user_data.cluster_time_slider, 'Position', [col1_pos-0.05 row1_pos-0.15 0.5 0.05 ]);
+    slider = jcontrol(dlg_user_data.window_h, dlg_user_data.cluster_time_slider, 'Position', [col1_pos-0.05 row1_pos-0.15 0.3 0.05 ]);
     set(slider, 'StateChangedCallback', @ClusterTimeChange);
     
     
@@ -55,12 +57,24 @@ function LoadingDialog(hObject, EventData)
     
     
     % buttons    
-    dlg_user_data.LoadButton=jcontrol(dlg_user_data.window_h, javax.swing.JButton('Load designated acquisition clusters'), 'Position', [0.55 row1_pos-0.15 0.4 0.05]);
+    dlg_user_data.LoadButton=jcontrol(dlg_user_data.window_h, javax.swing.JButton('Load designated acquisition clusters'), 'Position', [0.75 row1_pos-0.15 0.2 0.05]);
     set(dlg_user_data.LoadButton, 'MouseClickedCallback', @Load); 
+    
+    % checkbox
+    dlg_user_data.radiometric_splitting_checkbox = javax.swing.JCheckBox('Radiometric-based Splitting');
+    jcontrol(dlg_user_data.window_h, dlg_user_data.radiometric_splitting_checkbox, 'Position', [0.55 0.95 0.4 0.05]);
+    set(dlg_user_data.radiometric_splitting_checkbox, 'MouseClickedCallback', @RadSplitChange);
+    
        
     
     % get acquisition times
     [user_data.capture_times, user_data.capture_times_in_millis, user_data.capture_joda_times] = get_acquisition_times(user_data.specchio_client, user_data.level0_ids);
+    
+%     user_data.capture_times_in_millis_non_duplicated = user_data.capture_times_in_millis(1:2:end); % timestamps are duplicated due to dual channel data: reduce to one channel only
+    
+    user_data.capture_times_in_matlab_datenum = (user_data.capture_times_in_millis/1000/60/60/24) + datenum(1970,1,1,0,0,0);
+%     user_data.capture_times_in_matlab_datenum_non_duplicated = (user_data.capture_times_in_millis_non_duplicated/1000/60/60/24) + datenum(1970,1,1,0,0,0);
+    
     
     user_data.unique_cal_ids = unique(user_data.cal_ids);
     
@@ -100,18 +114,18 @@ function dlg_user_data=displayData(dlg_user_data)
     %user_data.level0_ids_sorted_by_time = tmp(ind);
     
     
-    user_data.capture_times_in_matlab_datenum = (user_data.capture_times_in_millis/1000/60/60/24) + datenum(1970,1,1,0,0,0);
+    
     
     
        
     % try determine the number of runs in this data set
-    user_data = determine_number_of_runs(user_data, dlg_user_data.cluster_time_slider.getValue(), false); % no plotting
+    user_data = determine_number_of_runs(user_data, dlg_user_data.cluster_time_slider.getValue(), false, dlg_user_data); % no plotting
     
     
     
     
     
-    dlg_user_data.report = create_report_table(user_data, dlg_user_data.window_h, [0 0 0.45 0.4]);
+    dlg_user_data.report = create_report_table(user_data, dlg_user_data.window_h, [0 0 0.3 0.4]);
     
     
     ColorSet = varycolor(length(user_data.tgb_run_info.runs));
@@ -124,7 +138,7 @@ function dlg_user_data=displayData(dlg_user_data)
     end
     
     hold(dlg_user_data.time_cluster_axes)
-    datetick(dlg_user_data.time_cluster_axes,'x','HH:mm')
+    datetick(dlg_user_data.time_cluster_axes,'x','HH:MM')
     xlabel(dlg_user_data.time_cluster_axes,'Time');
     ylabel(dlg_user_data.time_cluster_axes,'Spectra Count');  
     title(dlg_user_data.time_cluster_axes,'Time Clustering');  
@@ -137,7 +151,7 @@ function dlg_user_data=displayData(dlg_user_data)
 
     end
     hold(dlg_user_data.cal_cluster_axes)
-    datetick(dlg_user_data.cal_cluster_axes,'x','HH:mm')
+    datetick(dlg_user_data.cal_cluster_axes,'x','HH:MM')
     xlabel(dlg_user_data.cal_cluster_axes,'Time');
     ylabel(dlg_user_data.cal_cluster_axes,'Internal Calibration IDs');   
     title(dlg_user_data.cal_cluster_axes,'Calibration Clustering');  
@@ -146,6 +160,53 @@ function dlg_user_data=displayData(dlg_user_data)
     hold(dlg_user_data.combined_cluster_axes)
     
     title(dlg_user_data.combined_cluster_axes,'Combined Clustering');  
+    
+    
+    if dlg_user_data.radiometric_splitting_checkbox.isSelected()
+        
+        x = user_data.capture_times_in_matlab_datenum(1:2:end);
+        
+        tgts = dlg_user_data.run.ref_index == 0;
+        refs = dlg_user_data.run.ref_index == 1;
+        
+        
+        plot(dlg_user_data.rad_cluster_axes, x, dlg_user_data.run.ref_index);
+        hold(dlg_user_data.rad_cluster_axes)
+        
+        plot(dlg_user_data.rad_cluster_axes, x(tgts), dlg_user_data.run.ref_index(tgts), 'g*');
+        plot(dlg_user_data.rad_cluster_axes, x(refs), dlg_user_data.run.ref_index(refs), 'rx');
+        
+        % get breaks
+        breaks = [user_data.tgb_run_info.runs(:).end];
+        
+        break_bars = ones(size(breaks));
+        
+        bar(dlg_user_data.rad_cluster_axes, user_data.capture_times_in_matlab_datenum(breaks), break_bars, 0.1, 'r');
+        
+        
+        
+        break_bars = ones(size(user_data.run_info.missing_breaks));
+        
+%         bar(dlg_user_data.rad_cluster_axes, user_data.capture_times_in_matlab_datenum(user_data.run_info.missing_breaks), break_bars, 0.1, 'b');
+        
+        
+        xlabel(dlg_user_data.rad_cluster_axes, 'Time');
+        ylabel(dlg_user_data.rad_cluster_axes, 'Target or Reference');
+        title(dlg_user_data.rad_cluster_axes, 'Target and Reference Measurements over Time with Cluster Breaks');
+        legend(dlg_user_data.rad_cluster_axes, 'All Measurements Line', 'Targets', 'References', 'Breaks');
+        
+        datetick(dlg_user_data.rad_cluster_axes, 'x','HH:MM')
+        
+        hold(dlg_user_data.rad_cluster_axes)
+        
+        
+    else
+        
+        cla(dlg_user_data.rad_cluster_axes)
+        
+    end
+    
+    
      
     dlg_user_data.parent_user_data = user_data;
 
@@ -187,4 +248,46 @@ function Load(hObject, EventData)
 
     
 end
+
+
+function RadSplitChange(hObject, EventData)
+
+    
+    % get user data
+    fh = ancestor(hObject.hghandle, 'figure');    
+    dlg_user_data = get(fh, 'UserData');
+    
+    user_data=dlg_user_data.parent_user_data;
+    
+    
+    if dlg_user_data.radiometric_splitting_checkbox.isSelected()
+        
+        % load all data into one run
+        ColorSet = varycolor(1);
+        dlg_user_data.run = UniSpecDC_DataClass(user_data.specchio_client, user_data.level0_ids, ColorSet);
+        
+        
+        % 
+        dlg_user_data.run.setWvlsCoeffs(user_data.Instruments(user_data.current_instrument_index).coeffs_b);
+        dlg_user_data.run.load_from_db();
+        
+        dlg_user_data.run.wvlCalibration();
+        dlg_user_data.run.wvlInterpolation();        
+        dlg_user_data.run.splitGndSpectraIntoTgtAndRef();
+        
+        
+        % 
+        dlg_user_data=displayData(dlg_user_data);
+        
+        % store data in figure    
+        set(dlg_user_data.window_h, 'UserData', dlg_user_data);           
+        
+        
+    end
+    
+    
+
+end
+
+
 
--- determine_number_of_runs.m
+++ determine_number_of_runs.m
@@ -1,6 +1,14 @@
 
-function user_data = determine_number_of_runs(user_data, time_gap_minutes, do_plot)
+function user_data = determine_number_of_runs(user_data, time_gap_minutes, do_plot, dlg_user_data)
 
+    % state machine codes
+    global ref_panel
+    global target
+    
+    ref_panel = 1;
+    target = 2;
+    
+    
 
    % multiple instruments or calibrations: the calibration id is
     % a further criterion to cluster the data
@@ -23,7 +31,32 @@ function user_data = determine_number_of_runs(user_data, time_gap_minutes, do_pl
     % time_gap_based_runs
     tgb_run_index = 1;    
     tgb_runs(tgb_run_index).start = 1;
-    tgb_runs(tgb_run_index).cal_id = user_data.cal_ids(1);    
+    tgb_runs(tgb_run_index).cal_id = user_data.cal_ids(1);  
+    
+    
+    ref_index = 1;
+    
+    
+    
+    
+    if dlg_user_data.radiometric_splitting_checkbox.isSelected()
+        
+        if dlg_user_data.run.ref_index(1) == 1
+
+            state = ref_panel;
+            refs(ref_index).start = 1;
+            
+        else
+            
+            state = target;
+            
+        end        
+        
+    end
+    
+    
+    rad_index = 1;
+    rad_index_float = rad_index;
     
     
     for i=2:length(user_data.capture_times_in_millis)
@@ -32,36 +65,130 @@ function user_data = determine_number_of_runs(user_data, time_gap_minutes, do_pl
         
         cal_delta = user_data.cal_ids(i) - user_data.cal_ids(i-1);
         
-        if delta > time_gap_millis || cal_delta ~= 0
+        if dlg_user_data.radiometric_splitting_checkbox.isSelected()
             
-            runs(run_index).end = i-1;
+            % check for a change from target to panel
+%             if dlg_user_data.run.ref_index(i-1) == 0 && dlg_user_data.run.ref_index(i) == 1
+%                 
+%                 ref_panel_post = true;
+%                 ref_panel_pre = false;
+%                 target = false;
+%                                 
+%             end
+%             
+%             if dlg_user_data.run.ref_index(i-1) == 1 && dlg_user_data.run.ref_index(i) == 0
+%                 
+%                 %ref_panel_post = false;
+%                 ref_panel_pre = true;
+%                 target = true;
+%                                 
+%             end
+%             
+%             if dlg_user_data.run.ref_index(i-1) == 0 && dlg_user_data.run.ref_index(i) == 0
+%             
+%                 target = true;
+%             
+%             end
+%             
+%             diff = dlg_user_data.run.ref_index(i-1) - dlg_user_data.run.ref_index(i);
+%             
+%             if dlg_user_data.run.ref_index(i-1) == 1 && dlg_user_data.run.ref_index(i) == 0 && diff == 1 && ref_panel_post == true
+%             
+%                 ref_panel_pre_target_change = true;
+%                 ref_panel_post = false;
+%             
+%             else
+%                 
+%                 ref_panel_pre_target_change = false;
+%                 
+%             end
+
             
-            times(run_index).time = user_data.capture_times_in_millis(runs(run_index).start:runs(run_index).end);
-            matlab_times(run_index).time = user_data.capture_times_in_matlab_datenum(runs(run_index).start:runs(run_index).end);
+            [state, old_state] = state_machine(state, dlg_user_data.run.ref_index, rad_index);
             
-            run_index = run_index + 1;
-            
-            runs(run_index).start = i;
-            runs(run_index).cal_id = user_data.cal_ids(i);
-            
+           
+            if state == ref_panel && old_state == target
+                refs(ref_index).start = i; % first occurence
+            end
             
             
-        end
+            if state == target && old_state == ref_panel
+                refs(ref_index).end = i-1; % last occurence
+                ref_index = ref_index + 1;
+            end
+            
+            
+            
+            if (delta > time_gap_millis || cal_delta ~= 0) && state == ref_panel
+
+                runs(run_index).end = i-1;
+
+                times(run_index).time = user_data.capture_times_in_millis(runs(run_index).start:runs(run_index).end);
+                matlab_times(run_index).time = user_data.capture_times_in_matlab_datenum(runs(run_index).start:runs(run_index).end);
+
+                run_index = run_index + 1;
+
+                runs(run_index).start = i;
+                runs(run_index).cal_id = user_data.cal_ids(i);
+
+
+            end   
+            
+            
+            if delta > time_gap_millis && state == ref_panel
+
+                tgb_runs(tgb_run_index).end = i-1;
+
+                tgb_times(tgb_run_index).time = user_data.capture_times_in_millis(tgb_runs(tgb_run_index).start:tgb_runs(tgb_run_index).end);
+                tgb_matlab_times(tgb_run_index).time = user_data.capture_times_in_matlab_datenum(tgb_runs(tgb_run_index).start:tgb_runs(tgb_run_index).end);
+
+                tgb_run_index = tgb_run_index + 1;
+
+                tgb_runs(tgb_run_index).start = i;
+                tgb_runs(tgb_run_index).cal_id = user_data.cal_ids(i);
+
+
+            end             
+            
+            
+            rad_index_float = rad_index_float + 0.5; % deal with the duplication of time stamps by channels A and B by increasing the index by half the increment
+            
+            rad_index = floor(rad_index_float);
+            
+            
+        else
         
-        if delta > time_gap_millis
-            
-            tgb_runs(tgb_run_index).end = i-1;
-            
-            tgb_times(tgb_run_index).time = user_data.capture_times_in_millis(tgb_runs(tgb_run_index).start:tgb_runs(tgb_run_index).end);
-            tgb_matlab_times(tgb_run_index).time = user_data.capture_times_in_matlab_datenum(tgb_runs(tgb_run_index).start:tgb_runs(tgb_run_index).end);
-            
-            tgb_run_index = tgb_run_index + 1;
-            
-            tgb_runs(tgb_run_index).start = i;
-            tgb_runs(tgb_run_index).cal_id = user_data.cal_ids(i);
-                        
-            
-        end        
+            if delta > time_gap_millis || cal_delta ~= 0
+
+                runs(run_index).end = i-1;
+
+                times(run_index).time = user_data.capture_times_in_millis(runs(run_index).start:runs(run_index).end);
+                matlab_times(run_index).time = user_data.capture_times_in_matlab_datenum(runs(run_index).start:runs(run_index).end);
+
+                run_index = run_index + 1;
+
+                runs(run_index).start = i;
+                runs(run_index).cal_id = user_data.cal_ids(i);
+
+
+            end
+
+            if delta > time_gap_millis
+
+                tgb_runs(tgb_run_index).end = i-1;
+
+                tgb_times(tgb_run_index).time = user_data.capture_times_in_millis(tgb_runs(tgb_run_index).start:tgb_runs(tgb_run_index).end);
+                tgb_matlab_times(tgb_run_index).time = user_data.capture_times_in_matlab_datenum(tgb_runs(tgb_run_index).start:tgb_runs(tgb_run_index).end);
+
+                tgb_run_index = tgb_run_index + 1;
+
+                tgb_runs(tgb_run_index).start = i;
+                tgb_runs(tgb_run_index).cal_id = user_data.cal_ids(i);
+
+
+            end 
+        
+        end
     
     end
     
@@ -75,8 +202,86 @@ function user_data = determine_number_of_runs(user_data, time_gap_minutes, do_pl
     tgb_matlab_times(tgb_run_index).time = user_data.capture_times_in_matlab_datenum(runs(tgb_run_index).start:runs(tgb_run_index).end);
        
     
+    % check if there are reference blocks without splits. First and last
+    % blocks are OK, all others need splitting, by overlapping starts/ends
+    % to the neighbouring targets
+    
+    missing_breaks = [];
+    
+    if dlg_user_data.radiometric_splitting_checkbox.isSelected()
+        
+        missing_break_ind = 1;
+    
+            % get breaks
+        breaks = [runs(:).end];
+
+
+        for i=2:length(refs)-1
+            
+            index = refs(i).start <= breaks & refs(i).end >= breaks
+
+
+            if sum(index) == 0
+                
+                % missing break
+                missing_breaks(missing_break_ind) = round((refs(i).end + refs(i).start) / 2);
+                
+                missing_break_ind = missing_break_ind + 1;
+            end
+
+        end
+    
+    
+    end
+    
+    
+    user_data.run_info.times = times;
+    user_data.run_info.matlab_times = matlab_times;
+    user_data.run_info.runs = runs;
+    user_data.run_info.missing_breaks = missing_breaks;
+    user_data.cal_block_index = cal_block_index;
+    
+    user_data.tgb_run_info.times = tgb_times;
+    user_data.tgb_run_info.matlab_times = tgb_matlab_times;
+    user_data.tgb_run_info.runs = tgb_runs;    
+   
+    
+        
     
     if do_plot
+        
+        
+        figure
+        plot(user_data.capture_times_in_matlab_datenum(1:2:end), dlg_user_data.run.ref_index);
+        datetick('x','HH:MM')
+        
+        
+        figure
+        x = user_data.capture_times_in_matlab_datenum(1:2:end);
+        x = x(2:end);
+        y_input = user_data.capture_times_in_millis(1:2:end);
+        y = diff(y_input);
+        y = y/1000;
+        plot(x, y); 
+        
+        tgts = dlg_user_data.run.ref_index == 0;
+        refs = dlg_user_data.run.ref_index == 1;
+        
+        tgts = tgts(2:end);
+        refs = refs(2:end);
+        
+        hold
+        
+        plot(x(tgts), y(tgts), 'g*');
+        plot(x(refs), y(refs), 'rx');
+        
+        datetick('x','HH:MM')
+        xlabel('Time');
+        ylabel('Delta Time between Measurements [s]');
+        title('Target and Reference Measurements over Time');
+        
+        legend('All Measurements Line', 'Targets', 'References');
+        
     
         ColorSet = varycolor(run_index);
 %         figure
@@ -110,15 +315,51 @@ function user_data = determine_number_of_runs(user_data, time_gap_minutes, do_pl
     end
     
     
-    user_data.run_info.times = times;
-    user_data.run_info.matlab_times = matlab_times;
-    user_data.run_info.runs = runs;
-    user_data.cal_block_index = cal_block_index;
-    
-    user_data.tgb_run_info.times = tgb_times;
-    user_data.tgb_run_info.matlab_times = tgb_matlab_times;
-    user_data.tgb_run_info.runs = tgb_runs;    
-   
     
 
 end
+
+
+
+
+
+function [state, old_state] = state_machine(old_state, ref_index, i)
+
+    global ref_panel
+    global target
+
+    
+%     if ref_index(i-1) == 0 && ref_index(i) == 1
+% 
+%         state = ref_panel;
+% 
+%     elseif ref_index(i-1) == 0 && ref_index(i) == 0
+% 
+%         state = target;
+% 
+%     elseif ref_index(i-1) == 1 && ref_index(i) == 0
+% 
+%         state = target;
+%         
+%     elseif ref_index(i-1) == 1 && ref_index(i) == 1
+% 
+%         state = ref_panel;        
+% 
+%     end
+
+    if ref_index(i) == 1
+        
+        state = ref_panel;
+        
+    else
+        
+        state = target;
+        
+    end
+    
+
+end
+
+
+
+
